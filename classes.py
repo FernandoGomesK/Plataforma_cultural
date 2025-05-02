8dbf65c4292d2e5481a5ba92002e9ce71d0919b8
Fernando <fernandogomest94@gmail.com>
2025-05-02 15:53:41 -0400

    created the classes that inherit from person

---
+++ classes.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self):
+        pass
+    
+class Worker(Person):
+    def __init__(self):
+        pass
+    
+class Staff(Person):
+    def __init__(self):
+        pass
+    
+class Participant(Person):
+    def __init__(self):
+        super().__init__()
