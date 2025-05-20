89b15c7816d9aec35b77b6cdf970d1860866622b
Fernando <fernandogomest94@gmail.com>
2025-05-19 20:50:51 -0400

    added menu

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ class System():
     def run(self):
         while True:
             
+            print("1")
+            print("2")
+            print("3")
+            
             menu = input("select the option: ")
             if menu == "1":
                 self.ticket_mgr.show_ticket()
             elif menu == "2":
-                self.ticket_mgr.add_ticket()
+                self.ticket_mgr.add_ticket() 
             elif menu == "3":
                 self.ticket_mgr.remove_ticket()
             elif menu == "4":
