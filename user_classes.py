5d7531d0ecd65e26b0800c11ce7c062acc160647
Walter Camargo Neto <162071818+WC-Neto@users.noreply.github.com>
2025-05-29 14:24:57 -0400

    ft: two types of acess
    now you can register on our application like a admin if you're Organizer or Intermediary and not admin if you're just a user.

--- user_classes.py
+++ user_classes.py
@@ -22,8 +22,9 @@ class User(Person):
         return self.password == password
         
 class Organizer(Person):
-    def __init__(self, name: str, cpf: str, age: str, email: str, password: str, admin: bool = True):
+    def __init__(self, name: str, cpf: str, age: str, email: str, username:str, password: str, admin: bool = True):
         super().__init__(name, cpf, age, email)
+        self.username = username
         self.password = password
         self.admin = admin
         self.tickets: List[Ticket] = []
@@ -33,8 +34,9 @@ class Organizer(Person):
         
         
 class Intermediary(Person):
-    def __init__(self, name: str, cpf: str, age: str, email: str, password: str, admin: bool = True):
+    def __init__(self, name: str, cpf: str, age: str, email: str, username:str, password: str, admin: bool = True):
         super().__init__(name, cpf, age, email)
+        self.username = username
         self.password = password
         self.admin = admin
         self.tickets: List[Ticket] = []
